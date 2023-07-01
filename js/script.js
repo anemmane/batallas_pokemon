ccc8e5aea30809d692055173e222d9b88765f283
anemmane <24143981-anemmane@users.noreply.replit.com>
2023-07-01 18:48:14 +0000

    Se agrega funcion aleatorio para que el PC-enemigo seleccione un pokemon al azar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function iniciarJuego(){
+function iniciarJuego() {
   //accion para escuchar el boton de elegir pokemon. Primero creamos la variable    y le decimos que encuentre dentro del documento el elemento con determinado ID
 
   let botonElegirPokemon = document.getElementById('boton-seleccionar')
@@ -7,7 +7,7 @@ function iniciarJuego(){
   botonElegirPokemon.addEventListener('click', seleccionarPokemon)
 }
 
-function seleccionarPokemon(){
+function seleccionarPokemon() {
   let inputCharizard = document.getElementById('charizard')
   let inputBlastoise = document.getElementById('blastoise')
   let inputVenusaur = document.getElementById('venusaur')
@@ -22,8 +22,26 @@ function seleccionarPokemon(){
   } else{
     alert('Error en la seleccion')
   }
+
+  seleccionarPokemonEnemigo()
 }
 
+function seleccionarPokemonEnemigo(){
+  let pokemonAleatorio = aleatorio(1,3)
+  let pokemonEnemigo = document.getElementById('pokemonEnemigo')
+  
+  if (pokemonAleatorio == 1){
+    pokemonEnemigo.innerHTML = 'Charizard'
+  } else if (pokemonAleatorio == 2){
+    pokemonEnemigo.innerHTML = 'Blastoise'
+  } else if (pokemonAleatorio == 3){
+    pokemonEnemigo.innerHTML = 'Venusaur'
+  }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 
 window.addEventListener('load', iniciarJuego) // permite cargar el código a ejecutar cuando cargue todo el HTML
